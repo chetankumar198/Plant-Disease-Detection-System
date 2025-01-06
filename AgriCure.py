83b33320f34b1e96c1e2f0f5639ccb7067aae397
chetankumar198 <chetankumar8051@gmail.com>
2025-01-06 17:36:03 +0530

    Update AgriCure.py

--- AgriCure.py
+++ AgriCure.py
@@ -96,7 +96,7 @@ st.sidebar.title("🌱 Plant Disease Detection System")
 app_mode = st.sidebar.selectbox("Select Page", ["HOME", "DISEASE RECOGNITION"])
 
 # Display the image in the sidebar with shadow and glow
-img = Image.open(r"D:\Python\PlantDiseaseDetection\DALL·E 2025-01-03 09.39.38 - A vibrant and dynamic farm landscape in a 16_9 aspect ratio, with diverse healthy crops under a glowing sunset. Digital overlays display crop health a.webp")
+img = Image.open(r"DALL·E 2025-01-03 09.39.38 - A vibrant and dynamic farm landscape in a 16_9 aspect ratio, with diverse healthy crops under a glowing sunset. Digital overlays display crop health a.webp")
 st.sidebar.image(img, use_column_width=True, caption="Healthy Crops")
 
 # Class names for diseases
