3dcdb7f6dba23e4faf0b226c864da7e57f4ab10b
chetankumar198 <chetankumar8051@gmail.com>
2025-01-06 22:25:41 +0530

    Update AgriCure.py

--- AgriCure.py
+++ AgriCure.py
@@ -8,7 +8,7 @@ from tensorflow.keras.models import load_model
 # Load the pre-trained model once
 @st.cache_resource
 def load_prediction_model():
-    model_path = r"PlantDiseaseDetection/CNN_plantdiseases_model.keras filter=lfs diff=lfs merge=lfs -text"
+    model_path = r"CNN_plantdiseases_model.7z.002"
     if not os.path.exists(model_path):
         st.error(f"Model file not found at {model_path}. Please check the file path.")
         return None
